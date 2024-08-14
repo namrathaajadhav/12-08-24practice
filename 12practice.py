d8cdd5b27ee3c91837fd43406c8b7461169a0499
Namratha <namrathanjjadhav@gmail.com>
2024-08-14 09:03:00 +0530

    practice

---
+++ 12practice.py
@@ -0,0 +1,40 @@
+#oops concepts
+
+#instance avriable 
+class watch:
+    def __init__(self, brand, model) :
+      self.brand = brand
+      self.model = model
+allwatch = watch("titan", "smartwatch")
+print(allwatch.brand)
+print(allwatch.model)
+
+
+
+#static variable
+class Gold :
+   puregold = " malabar gold "
+   def __init__(self) :
+      Gold.shop = "Chenni"
+      def isinstancemethod(self):
+         Gold.type = "Diamonds"
+         @classmethod
+
+         def classmethods(cls):
+          cls.price = 100,000
+
+          def staticmethods():
+            Gold.jewellery = "ear rings"
+Hall=Gold()
+print(Hall.shop)
+print(Gold.__dict__)
+
+      
+#local variable
+class message:
+    name = "Hello rahul"
+    print(name)
+
+        
+
+
